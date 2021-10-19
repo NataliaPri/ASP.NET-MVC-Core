795693948e90a7e9fa4bf1c394b2b5d36f776161
NataliaPri <77389496+NataliaPri@users.noreply.github.com>
2021-10-19 19:33:55 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ public class Example
     {
         public static void Main()
         {
-            // Queue the task.
             ThreadPool.QueueUserWorkItem(ThreadProc);
             Console.WriteLine("Main thread does some work, then sleeps.");
             Thread.Sleep(1000);
@@ -13,14 +12,11 @@ public class Example
             Console.WriteLine("Main thread exits.");
         }
 
-        // This thread procedure performs the task.
         static void ThreadProc(Object stateInfo)
         {
-            // No state object was passed to QueueUserWorkItem, so stateInfo is null.
             Console.WriteLine("Hello from the thread pool.");
         }
     }
-    // The example displays output like the following:
     //       Main thread does some work, then sleeps.
     //       Hello from the thread pool.
     //       Main thread exits.
