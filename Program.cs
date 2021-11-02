e3e3c236f84a716f3f9b6f9c040698446b0a2f7b
NataliaPri <77389496+NataliaPri@users.noreply.github.com>
2021-11-02 18:08:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        public sealed class Product
+        {
+            public string Name { get; set; }
+            public int Price { get; set; }
+            public string quantity { get; set; }
+        }
+
+    public interface Basket
+        {
+            IReadOnlyList<Product> GetAll();
+        }
+
+
+    }
+    };
